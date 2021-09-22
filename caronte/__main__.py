f55abc7d716dfe81a2ffc409d1e47853c26408a8
Lorenzo Balugani <lorenzo.balugani@gmail.com>
2021-09-22 15:59:51 +0200

    Bot is now feature complete.
    Requires extensive testing.

--- caronte/__main__.py
+++ caronte/__main__.py
@@ -4,9 +4,9 @@ import asyncio
 import os
 from telepot.aio.loop import MessageLoop
 from telepot.aio.delegate import pave_event_space, per_chat_id, create_open
-from database.models import User, Base
-from utils import ChatModes
-from database.db import SessionLocal, engine
+from caronte.database.models import User, Base
+from caronte.utils import ChatModes, create_token, send_email_sb
+from caronte.database.db import SessionLocal, engine
 
 Base.metadata.create_all(bind=engine)
 
@@ -31,7 +31,7 @@ class CharonProgram(telepot.aio.helper.ChatHandler):
         else:
             await self.sender.sendMessage("Comando sconosciuto.")
 
-    async def on_chat_message(self, msg):
+    async def private_chat_handler(self, msg):
         if not self.uname and msg['chat']['type'] == "private":
             with SessionLocal() as db:
                 db: sqlalchemy.orm.Session
@@ -64,11 +64,17 @@ class CharonProgram(telepot.aio.helper.ChatHandler):
             if "entities" not in msg.keys() or msg["entities"][0]['type'] != "email":
                 await self.sender.sendMessage("Email non valida. Riprova.")
                 return
-            # Check for email compliancy
-            # Create and send the token to email
-            self.token = "1234"
+            left, right = msg['text'].split("@")
+            if right != domain:
+                await self.sender.sendMessage(
+                    "L'email non appartiene al dominio indicato.\nPer favore, usa la tua mail istituzionale.")
+                return
+            self.token = create_token(6)
             self.email = msg['text']
-            await self.sender.sendMessage("E' stato mandato un codice di verifica a {}. Mandamelo entro 5 minuti.\n\nPer interrompere la procedura, fai /start.".format(msg['text']))
+            await send_email_sb(email=self.email, token=self.token)
+            await self.sender.sendMessage(
+                "E' stato mandato un codice di verifica a {}. Mandamelo entro 5 minuti.\n\nPer interrompere la procedura, fai /start.".format(
+                    msg['text']))
             self.mode = ChatModes.AUTH_TOKEN
             return
         elif self.mode == ChatModes.AUTH_BEGIN:
@@ -79,13 +85,100 @@ class CharonProgram(telepot.aio.helper.ChatHandler):
                 return
 
             await self.sender.sendMessage(
-                "Inserisci la tua email istituzionale (deve terminare per {}).\n\nPer interrompere la procedura, fai /start.".format(
+                "Inserisci la tua email istituzionale (deve terminare per {}). \n\nPer interrompere la procedura, fai /start.".format(
                     domain))
             self.mode = ChatModes.AUTH_WAIT
             return
+        elif self.mode == ChatModes.VISIBILITY:
+            with SessionLocal() as db:
+                db: sqlalchemy.orm.Session
+                user = db.query(User).filter_by(chatid=str(msg['chat']['id'])).first()
+                if not user:
+                    await self.sender.sendMessage("Utente non autenticato.\nImpossibile modificare le impostazioni per un utente ignoto.")
+                    self.mode = ChatModes.NONE
+                    return
+                user.hidden = not user.hidden
+                db.commit()
+                if user.hidden:
+                    await self.sender.sendMessage("Modalità anonima attivata.")
+                else:
+                    await self.sender.sendMessage("Modalità anonima disattivata.")
         elif self.mode == ChatModes.NONE:
             await self.sender.sendMessage(
-                "Ciao,\nSono Caronte, il bot di Telegram per l'autenticazione e l'accesso a chat riservate.\n\nClicca su /auth per autenticarti, se vuoi cambiare le tue impostazioni di visibilità clicca su /visibility.")
+                "Ciao,\nSono Caronte, il bot di Telegram per l'autenticazione e l'accesso a chat riservate. \n\nClicca su /auth per autenticarti, se vuoi cambiare le tue impostazioni di visibilità clicca su /visibility.\nPer accedere all'indice dei gruppi, clicca sul seguente link: \n{}".format(
+                    os.getenv("INDEX_URL")))
+
+    async def public_chat_handler(self, msg):
+        if "entities" in msg.keys() and msg['entities'][0]['type'] == "bot_command":
+            text = msg['text'].split(" ")
+            if text[0] == "/whois":
+                if len(text) == 3:
+                    await self.whois_name_surname(text)
+                elif len(text) == 2:
+                    await self.whois_username(text)
+                else:
+                    await self.sender.sendMessage("Sintassi con corretta.\n/whois [Nome] [Cognome]\n/whois [@username]")
+
+    async def whois_username(self, text):
+        username = text[1][1:]
+        with SessionLocal() as db:
+            user = db.query(User).filter_by(username=username).first()
+            if user.hidden:
+                await self.sender.sendMessage("Nessun utente pubblico è stato trovato.")
+            else:
+                await self.sender.sendMessage("Risultato ricerca:\n{} {}, @{}".format(user.name, user.surname, user.username))
+
+    async def whois_name_surname(self, text):
+        name = text[1].lower()
+        surname = text[2].lower()
+        with SessionLocal() as db:
+            users = db.query(User).filter_by(name=name, surname=surname).all()
+            s = "Risultato ricerca:"
+            for user in users:
+                if user.hidden:
+                    s += ""
+                else:
+                    s += "\n{} {}, @{}".format(user.name, user.surname, user.username)
+            if s == "Risultato ricerca:":
+                await self.sender.sendMessage("Nessun utente pubblico è stato trovato.")
+            else: await self.sender.sendMessage(s)
+
+    async def on_chat_message(self, msg):
+        if msg['chat']['type'] == "private":
+            await self.private_chat_handler(msg)
+        else:
+            g = telepot.glance(msg)
+            if g[0] == 'new_chat_member':
+                await self.new_user_handler(msg)
+            await self.public_chat_handler(msg)
+
+    async def on_close(self, ex):
+        pass
+
+    async def new_user_handler(self, msg):
+        user = msg["new_chat_member"]['username']
+        with SessionLocal() as db:
+            user = db.query(User).filter_by(username=user).first()
+            if not user:
+                await self.hammer(msg["new_chat_member"])
+                return
+            if user.hidden:
+                await self.sender.sendMessage(
+                    "L'utente che si è collegato è autenticato, ma ha deciso di rimanere anonimo.\nBenvenuto!")
+            else:
+                await self.sender.sendMessage(
+                    "L'utente che si è collegato è {} {}.\nBenvenuto!".format(user.name, user.surname))
+
+    async def hammer(self, member):
+        user = member['username']
+        try:
+            await self.sender.sendMessage("L'utente non è autenticato, e verrà rimosso...")
+            await bot.kickChatMember(self.chat_id, member['id'])
+        except telepot.exception.NotEnoughRightsError:
+            await self.sender.sendMessage("Impossibile rimuovere l'utente {} per mancanza di permessi.".format(user))
+        except telepot.exception.TelegramError:
+            await self.sender.sendMessage(
+                "Impossibile rimuovere l'utente {} a causa di un errore generico.".format(user))
 
 
 bot_token = os.getenv("TOKEN")
--- caronte/utils.py
+++ caronte/utils.py
@@ -1,5 +1,10 @@
-from database.db import SessionLocal
+import os
+
+from caronte.database.db import SessionLocal
 from enum import Enum
+import random
+import string
+import requests
 
 
 def get_db():
@@ -13,3 +18,20 @@ class ChatModes(Enum):
     AUTH_WAIT = 2
     AUTH_TOKEN = 3
     VISIBILITY = 4
+
+
+def create_token(size):
+    return "".join(random.choice(string.ascii_uppercase) for i in range(size))
+
+
+async def send_email_sb(email, token):
+    r = requests.post(url="https://api.sendinblue.com/v3/smtp/email", headers={
+        'accept': 'application/json',
+        'api-key': os.getenv("SB_API"),
+        'content-type': 'application/json'
+    }, json={
+        'sender': {'name': 'Caronte', 'email': 'noreply@fermitech.info'},
+        'to': [{'email': email}],
+        'subject': "Caronte - Token usa e getta",
+        'htmlContent': token
+    })
